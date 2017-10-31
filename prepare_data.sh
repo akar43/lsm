9fa9ada9a1643433311ee7af829dff2ad576bef8
Abhishek Kar <abhishekkar43@gmail.com>
2017-10-31 16:16:02 -0700

    add temporary url

--- prepare_data.sh
+++ prepare_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -O data/shapenet_release.tar.gz <url>
+wget -O data/shapenet_release.tar.gz http://people.eecs.berkeley.edu/~akar/lsm/shapenet_release.tar.gz
 cd data
 tar -xvzf shapenet_release.tar.gz
 cd ..
